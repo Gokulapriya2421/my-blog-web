1dd32aacfdecb409bf9158753302bd7b332b1109
gokulapriya <gokulapriyasekar2001@gmail.com>
2021-04-10 19:37:00 +0530

    added view and template for detailed blog post as well as CSS for the site.

--- myBlogApp/urls.py
+++ myBlogApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 
 ]
--- myBlogApp/views.py
+++ myBlogApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpRequest,HttpResponse
 from .models import Post
 import datetime
@@ -6,3 +6,6 @@ from django.utils import timezone
 def home(request):
     posts = Post.objects.filter(published_date__lte= timezone.now()).order_by('published_date')
     return render(request, './home.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
